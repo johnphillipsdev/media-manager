dd201b0938fe1635fd4f49b6940640dc2215cd03
John <development@johnphillips.dev>
2023-09-02 20:30:34 +0100

    tidy up on component

--- components/Navigation/Sidebar/SidebarHeading.tsx
+++ components/Navigation/Sidebar/SidebarHeading.tsx
@@ -5,18 +5,7 @@ type Props = {
 export default function SidebarHeading({ label }: Props) {
   return (
     <li className='px-5 mt-4 mb-2'>
-      <div className='flex flex-row items-center'>
-        <div className='text-sm font-light tracking-wide text-gray-500'>
-          {label}
-        </div>
-      </div>
+      <p className='text-sm font-light tracking-wide text-gray-500'>{label}</p>
     </li>
   );
 }
-
-/**
- * TODO LIST
- * - cleanup tailwind styles
- * - remove useless elements
- * - change styling
- */
